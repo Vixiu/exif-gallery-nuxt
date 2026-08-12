6c346e3c3878f8652d632c525d034b53cc5e07da
Vixiu <93539736+Vixiu@users.noreply.github.com>
2026-08-12 23:33:57 +0800

    feat: protect R2 photo assets with gallery password

--- server/routes/photos/[pathname].get.ts
+++ server/routes/photos/[pathname].get.ts
@@ -1,7 +1,9 @@
 import { blob } from '@nuxthub/blob'
 import { z } from 'zod'
+import { requireGalleryAccess } from '../../utils/galleryAccess'
 
 export default eventHandler(async (event) => {
+  await requireGalleryAccess(event)
   const { pathname } = await getValidatedRouterParams(event, z.object({
     pathname: z.string().min(1),
   }).parse)
